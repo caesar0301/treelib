85738fbcc6662abb1296dc94191d117edcdb5105
caesar0301 <chenxm35@gmail.com>
2013-12-22 17:40:32 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ from treelib import __version__
 setup(
     name = "treelib",
     version = __version__,
-    url = '',
+    url = 'https://github.com/caesar0301/pyTree',
     author = 'caesar0301',
     author_email = 'chenxm35@gmail.com',
     description = 'A Python 2/3 implementation of tree structure.',
     long_description='''This is a simple tree data structure implementation in python.''',
     license = "LICENSE",
     packages = ['treelib'],
+    keywords = ['data structure', 'tree', 'tools'],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
