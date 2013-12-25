f89e368cd494c5dba0552f605b2c68ae21a862a6
jaminchen <chenxm35@gmail.com>
2013-12-25 15:56:17 +0800

    add .travis.yml; update test_treelib.py

--- tests/test_treelib.py
+++ tests/test_treelib.py
@@ -1,29 +1,47 @@
+#!/usr/bin/env python
 import unittest
 from treelib import Tree, Node
 
 class NodeCase(unittest.TestCase):
-	def setUp(self):
-		self.node1 = Node("Test One", "ide ntifier 1 ")
+    def setUp(self):
+        self.node1 = Node("Test One", "identifier 1")
+        self.node2 = Node("Test Two", "identifier 2")
 
-	def test_initialization(self):
-		self.assertEqual(self.node1.tag, "Test One")
-		self.assertEqual(self.node1.identifier, "ide_ntifier_1")
-		self.assertEqual(self.node1.expanded, True)
+    def test_initialization(self):
+        self.assertEqual(self.node1.tag, "Test One")
+        self.assertEqual(self.node1.identifier, "identifier 1")
+        self.assertEqual(self.node1.expanded, True)
+        self.assertEqual(self.node1.bpointer, None)
+        self.assertEqual(self.node1.fpointer, [])
 
-	def test_set_fpointer(self):
-		self.node1.update_fpointer(" identi fier 2")
-		self.assertEqual(self.node1.fpointer, ['identi_fier_2'])
+    def test_set_tag(self):
+        self.node1.tag = "Test 1"
+        self.assertEqual(self.node1.tag, "Test 1")
+        self.node1.tag = "Test One"
 
-	def test_set_bpointer(self):
-		self.node1.bpointer = " identi fier  1"
-		self.assertEqual(self.node1.bpointer, 'identi_fier__1')
+    def test_set_identifier(self):
+        self.node1.identifier = "ID1"
+        self.assertEqual(self.node1.identifier, "ID1")
+        self.node1.identifier = "identifier 1"
 
-	def test_set_data(self):
-		self.node1.data = {1:'hello', "two":'world'}
-		self.assertEqual(self.node1.data, {1:'hello', "two":'world'})
+    def test_set_fpointer(self):
+        self.node1.update_fpointer("identifier 2")
+        self.assertEqual(self.node1.fpointer, ['identifier 2'])
+        self.node1.fpointer = []
 
-	def tearDown(self):
-		pass
+    def test_set_bpointer(self):
+        self.node2.update_bpointer("identifier 1")
+        self.assertEqual(self.node2.bpointer, 'identifier 1')
+        self.node2.bpointer = None
+
+    def test_set_is_leaf(self):
+        self.node1.update_fpointer("identifier 2")
+        self.node2.update_bpointer("identifier 1")
+        self.assertEqual(self.node1.is_leaf(), False)
+        self.assertEqual(self.node2.is_leaf(), True)
+
+    def tearDown(self):
+        pass
 
 
 class TreeCase(unittest.TestCase):
@@ -33,33 +51,39 @@ class TreeCase(unittest.TestCase):
         tree.create_node("Jane", "jane", parent="harry")
         tree.create_node("Bill", "bill", parent="harry")
         tree.create_node("Diane", "diane", parent="jane")
-        tree.create_node("George", "george", parent="diane")
-        tree.create_node("Mary", "mary", parent="diane")
-        tree.create_node("Jill", "jill", parent="george")
-        tree.create_node("Mark", "mark", parent="jane")
+        tree.create_node("George", "george", parent="bill")
         self.tree = tree
+        self.copytree = Tree(self.tree, True)
 
     def test_tree(self):
-        self.assertIsInstance(self.tree, Tree)
+        self.assertEqual(isinstance(self.tree, Tree), True)
+        self.assertEqual(isinstance(self.copytree, Tree), True)
+
+    def test_nodes(self):
+        self.assertEqual(len(self.tree.nodes), 5)
+        self.assertEqual(len(self.tree.all_nodes()), 5)
+        self.assertEqual(self.tree.size(), 5)
 
     def test_getitem(self):
         """Nodes can be accessed via getitem."""
-        tree = Tree()
-        tree.create_node("Harry", "harry")
-        tree.create_node("Jane", "jane", parent="harry")
-
-        for node_id in tree.nodes:
+        for node_id in self.tree.nodes:
             try:
-                tree[node_id]
+                self.tree[node_id]
             except KeyError:
                 self.fail('Node access should be possible via getitem.')
-
         try:
-            tree['root']
+            self.tree['root']
         except KeyError:
             pass
         else:
             self.fail('There should be no default fallback value for getitem.')
+
+    def test_parent(self):
+        for nid in self.tree.nodes:
+            if nid == self.tree.root:
+                self.assertEqual(self.tree.parent(nid), None)
+            else:
+                self.assertEqual(self.tree.parent(nid) in self.tree.all_nodes(), True)
 
     def tearDown(self):
         pass
