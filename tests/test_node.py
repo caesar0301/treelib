4dde1fd833bb69a5e8375072575c4c091308ad60
Leonard Binet <leonardbinet@gmail.com>
2019-10-12 16:51:53 +0200

    Tree wise node pointers, tree inheritance capabilities
    nodes with  bpointer/fpointers per tree
    
    unicode fix on repr
    
    deep copy of fpointer list
    
    fix python 2-3 compatibility
    
    remove no-longer available versions on travis
    
    copy method to avoid having to recopy whole subtree/remove_subtree methods while adding init attributes
    
    get instance method intended for overloading
    
    rename get_instance -> _get_instance
    
    fix paste
    
    keep retro-compatility on public methods and attributes
    
    rename '_get_instance' -> '_clone'
    
    print optional in 'show' method

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,5 +1,6 @@
 import unittest
 
+from collections import defaultdict
 from treelib import Node
 
 
@@ -11,9 +12,13 @@ class NodeCase(unittest.TestCase):
     def test_initialization(self):
         self.assertEqual(self.node1.tag, "Test One")
         self.assertEqual(self.node1.identifier, "identifier 1")
-        self.assertEqual(self.node1.expanded, True)
+        # retro-compatibility
         self.assertEqual(self.node1.bpointer, None)
         self.assertEqual(self.node1.fpointer, [])
+
+        self.assertEqual(self.node1.expanded, True)
+        self.assertEqual(self.node1._bpointer, {})
+        self.assertEqual(self.node1._fpointer, defaultdict(list))
         self.assertEqual(self.node1.data, None)
 
     def test_set_tag(self):
@@ -32,20 +37,44 @@ class NodeCase(unittest.TestCase):
         self.node1.identifier = "identifier 1"
 
     def test_set_fpointer(self):
+        # retro-compatibility
         self.node1.update_fpointer("identifier 2")
         self.assertEqual(self.node1.fpointer, ['identifier 2'])
         self.node1.fpointer = []
+        self.assertEqual(self.node1.fpointer, [])
+
+    def test_set_fpointer_in_tree(self):
+        self.node1.update_fpointer_in_tree("tree 1", "identifier 2")
+        self.assertEqual(self.node1.fpointer_in_tree("tree 1"), ['identifier 2'])
+        self.assertEqual(self.node1._fpointer["tree 1"], ['identifier 2'])
+        self.node1.set_fpointer_in_tree("tree 1", [])
+        self.assertEqual(self.node1._fpointer["tree 1"], [])
 
     def test_set_bpointer(self):
+        # retro-compatibility
         self.node2.update_bpointer("identifier 1")
         self.assertEqual(self.node2.bpointer, 'identifier 1')
         self.node2.bpointer = None
+        self.assertEqual(self.node2.bpointer, None)
+
+    def test_set_bpointer_in_tree(self):
+        self.node2.update_bpointer_in_tree("tree 1", "identifier 1")
+        self.assertEqual(self.node2.bpointer_in_tree("tree 1"), 'identifier 1')
+        self.assertEqual(self.node2._bpointer["tree 1"], 'identifier 1')
+        self.node2.update_bpointer_in_tree("tree 1", None)
+        self.assertEqual(self.node2.bpointer_in_tree("tree 1"), None)
 
     def test_set_is_leaf(self):
         self.node1.update_fpointer("identifier 2")
         self.node2.update_bpointer("identifier 1")
         self.assertEqual(self.node1.is_leaf(), False)
         self.assertEqual(self.node2.is_leaf(), True)
+
+    def test_set_is_leaf_in_tree(self):
+        self.node1.update_fpointer_in_tree("tree 1", "identifier 2")
+        self.node2.update_bpointer_in_tree("tree 1", "identifier 1")
+        self.assertEqual(self.node1.is_leaf_in_tree("tree 1"), False)
+        self.assertEqual(self.node2.is_leaf_in_tree("tree 1"), True)
 
     def test_data(self):
 
