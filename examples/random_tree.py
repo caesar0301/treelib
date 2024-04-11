ee1b1f695428efd6da11461a9434091713e16c7c
William Song <30965609+Freakwill@users.noreply.github.com>
2024-04-11 14:52:01 +0800

    Update random_tree.py

--- examples/random_tree.py
+++ examples/random_tree.py
@@ -1,10 +1,11 @@
 import random
 from treelib import Tree
 
+
 def _random(max_depth=5, min_width=1, max_width=2, offset=()):
     # generate a tree randomly
     tree = Tree()
-    root = tree.create_node(identifier=offset)
+    tree.create_node(identifier=offset)
     if max_depth == 0:
         return tree
     elif max_depth == 1:
