f5daf2dd38c7be49e50fcba52aca232350661295
xiaming.chen <chenxm35@gmail.com>
2025-03-02 11:34:00 +0800

    revert py3.14

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
